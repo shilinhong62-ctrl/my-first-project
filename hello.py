733cab5c593c530a26faa383bdd53de7d3063d1a
shilinhong62-ctrl <shilinhong62@gmail.com>
2026-02-01 15:16:53 +0800

    two commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+# hello.py
+# 我的第一个Python脚本
+
+def greet(name):
+    """打招呼函数"""
+    return f"Hello, {name}!"
+
+def main():
+    """主函数"""
+    print(greet("World"))
+    print(greet("github"))
+if __name__ == "__main__":  
+    main()
